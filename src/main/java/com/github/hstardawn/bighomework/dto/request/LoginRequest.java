484ce9c5e11e27dd06a67b4ebbde487141c7d997
SugarMGP <2350745751@qq.com>
2025-09-05 16:31:02 +0800

    Update LoginRequest.java

--- src/main/java/com/github/hstardawn/bighomework/dto/request/LoginRequest.java
+++ src/main/java/com/github/hstardawn/bighomework/dto/request/LoginRequest.java
@@ -10,12 +10,9 @@ import jakarta.validation.constraints.Size;
 @Data
 @AllArgsConstructor
 public class LoginRequest {
-    @Size(max = 20)
     @NotBlank
-    @Pattern(regexp = "^[0-9]+$")
     private String username;
 
     @NotBlank
-    @Size(min = 8, max = 16)
     private String password;
 }
